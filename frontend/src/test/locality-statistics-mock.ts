74f53f23f5e37851da6bfb0cd0babbec1b63fba8
Tiago Parra <tiago@jobzi.com>
2022-11-30 13:53:23 -0300

    #22 Locality statistics model unit tests

---
+++ frontend/src/test/locality-statistics-mock.ts
@@ -0,0 +1,67 @@
+export const localityStatisticsFromServer = [
+  {
+    "id": 1,
+    "locality_map_id": 2,
+    "municipalities_count": 100,
+    "regions_count": 1,
+    "states_count": 5,
+    "school_count": 24781,
+    "student_count": 4727271,
+    "connected_count": 7897,
+    "connected_percentage": 31.8671562890924,
+    "internet_availability_null_count": 4352,
+    "internet_availability_null_percentage": 17.5618417335862,
+    "internet_availability_by_value": {
+      "Yes": 7897,
+      "No": 12532,
+      "NA": 4352
+    },
+    "internet_availability_by_school_region": {
+      "Rural": {
+        "NA": 3933,
+        "No": 11228,
+        "Yes": 2357
+      },
+      "Urban": {
+        "NA": 419,
+        "No": 1304,
+        "Yes": 5540
+      }
+    },
+    "internet_availability_by_school_type": {
+      "Estadual": {
+        "NA": 275,
+        "No": 1583,
+        "Yes": 2371
+      },
+      "Federal": {
+        "NA": 1,
+        "No": 0,
+        "Yes": 79
+      },
+      "Municipal": {
+        "NA": 4076,
+        "No": 10949,
+        "Yes": 5447
+      }
+    },
+    "internet_availability_prediction_count": 407,
+    "internet_availability_prediction_percentage": 9.35202205882353,
+    "create_at": "2022-11-25T20:14:15.001452+00:00",
+    "locality_map": {
+      "id": 2,
+      "country_name": "Brasil",
+      "country_code": "BR",
+      "region_name": "Norte",
+      "region_code": "1",
+      "state_name": null,
+      "state_abbreviation": null,
+      "state_code": null,
+      "municipality_name": null,
+      "municipality_code": null,
+      "adm_level": "region",
+      "geometry": {},
+      "create_at": "2022-11-23T19:58:42.418144+00:00"
+    }
+  }
+]
