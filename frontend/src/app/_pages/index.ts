07e938dcac6476b6acdfe92893fc415d25010c08
Tiago Parra <tiago@jobzi.com>
2022-12-28 10:54:47 -0300

    #51 Add initial component files and routing mapping

--- frontend/src/app/_pages/index.ts
+++ frontend/src/app/_pages/index.ts
@@ -4,3 +4,4 @@ export * from './home/home.component';
 export * from './data-source-reference/data-source-reference.component';
 export * from './interactive-map/interactive-map.component';
 export * from './license/license.component';
+export * from './page-not-found/page-not-found.component';
