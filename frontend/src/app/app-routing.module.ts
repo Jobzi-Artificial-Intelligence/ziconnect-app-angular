da52ec47ccc457f4de453004a09b8b20ebc36c02
Tiago Parra <tiago@jobzi.com>
2023-02-09 14:44:01 -0300

    #70 Removed all references to google-maps library

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import {
   ContributorsComponent,
   DataSourceReferenceComponent,
   HomeComponent,
-  InteractiveMapComponent,
   InteractiveOsmMapComponent,
   LicenseComponent,
   PageNotFoundComponent
