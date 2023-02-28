2cef0b91cb5383db92f966afa6ef32018de69ab3
Tiago Parra <tiago@jobzi.com>
2023-02-28 08:54:47 -0300

    #77 Added analysis type selection component

--- frontend/src/app/_pages/analysis-tool/analysis-tool.component.ts
+++ frontend/src/app/_pages/analysis-tool/analysis-tool.component.ts
@@ -1,5 +1,7 @@
 import { HttpEventType, HttpResponse } from '@angular/common/http';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AnalysisType } from 'src/app/_helpers/enums/analysis-type';
+import { AnalysisTask } from 'src/app/_models';
 import { AlertService, AnalysisToolService } from 'src/app/_services';
 
 @Component({
@@ -9,24 +11,31 @@ import { AlertService, AnalysisToolService } from 'src/app/_services';
 })
 export class AnalysisToolComponent implements OnInit {
   @ViewChild('sectionTypeSelection') sectionTypeSelection: ElementRef | undefined;
+  @ViewChild('fileDropRef') fileDropRef: ElementRef | undefined;
 
+  public selectedAnalysisType: AnalysisType | undefined = undefined;
   public selectedFile: File | undefined;
   public responseBody: any;
   public progress: number = 0;
-  public predictionTaskId: string = '';
+  public storageTask?: AnalysisTask | null = null;
 
   constructor(private _alertService: AlertService, private _analysisToolService: AnalysisToolService) { }
 
   ngOnInit(): void {
     this.selectedFile = undefined;
-
-    this.loadPredictionTaskId();
   }
 
-  loadPredictionTaskId() {
-    const storageTaskId = localStorage.getItem('prediction_task_id');
+  loadStorageTask() {
+    if (this.selectedAnalysisType) {
+      this.storageTask = null;
+      const analysisTypeStr = AnalysisType[this.selectedAnalysisType];
 
-    this.predictionTaskId = storageTaskId ? storageTaskId : '';
+      const storageTask = localStorage.getItem(`${analysisTypeStr}_task`);
+
+      if (storageTask) {
+        this.storageTask = JSON.parse(storageTask);
+      }
+    }
   }
 
   //#region Upload Files Handlers
@@ -52,19 +61,36 @@ export class AnalysisToolComponent implements OnInit {
 
   onButtonRemoveFileClick() {
     this.selectedFile = undefined;
+    if (this.fileDropRef) {
+      this.fileDropRef.nativeElement.value = '';
+    }
+
   }
 
-  onButtonSetStorageValueClick() {
-    const newValue = Math.random().toString(36).substring(2, 12);
-    localStorage.setItem('prediction_task_id', newValue);
+  setStorageValueClick() {
+    if (this.selectedAnalysisType) {
+      const newValue = Math.random().toString(36).substring(2, 12);
+      const analysisTypeStr = AnalysisType[this.selectedAnalysisType];
 
-    this.predictionTaskId = newValue;
+      const task = new AnalysisTask();
+      task.taskId = newValue;
+
+      localStorage.setItem(`${analysisTypeStr}_task`, JSON.stringify(task));
+
+      this.storageTask = task;
+    }
+
   }
 
   onButtonRemoveStorageValueClick() {
-    localStorage.removeItem('prediction_task_id');
+    if (this.selectedAnalysisType) {
+      const analysisTypeStr = AnalysisType[this.selectedAnalysisType];
 
-    this.predictionTaskId = '';
+
+      localStorage.removeItem(`${analysisTypeStr}_task`);
+
+      this.onSelectAnalysisTypeClick(this.selectedAnalysisType);
+    }
   }
 
   onButtonStartAnalysisClick() {
@@ -74,6 +100,8 @@ export class AnalysisToolComponent implements OnInit {
     }
 
     this.progress = 0;
+
+    this.setStorageValueClick();
 
     this._analysisToolService
       .postNewPredictionAnalysis(this.selectedFile)
@@ -88,6 +116,13 @@ export class AnalysisToolComponent implements OnInit {
       });
   }
 
+  onSelectAnalysisTypeClick(type: AnalysisType) {
+    this.selectedAnalysisType = type;
+    this.initNewAnalysis();
+
+    this.loadStorageTask();
+  }
+
   scrollToStartAnalysisSection(): void {
     console.log(this.sectionTypeSelection);
     if (this.sectionTypeSelection) {
@@ -96,6 +131,15 @@ export class AnalysisToolComponent implements OnInit {
       const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
 
       window.scrollTo({ top: y, behavior: 'smooth' });
+    }
+  }
+
+  initNewAnalysis() {
+    this.selectedFile = undefined;
+    this.storageTask = null;
+
+    if (this.fileDropRef) {
+      this.fileDropRef.nativeElement.value = '';
     }
   }
 }
--- frontend/src/app/material.module.ts
+++ frontend/src/app/material.module.ts
@@ -5,6 +5,7 @@ import { MatAutocompleteModule } from "@angular/material/autocomplete";
 import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatExpansionModule } from '@angular/material/expansion';
@@ -29,6 +30,7 @@ const materialModules = [
   MatBottomSheetModule,
   MatButtonModule,
   MatButtonToggleModule,
+  MatCardModule,
   MatCheckboxModule,
   MatDialogModule,
   MatExpansionModule,
